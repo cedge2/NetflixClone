a27594e6a3da3d4d8034053f0cf6b3f96f7fac47
Camber <cedge2@asu.edu>
2024-09-08 19:58:53 -0500

    Set up database with MongoDB

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,32 @@
+import { User } from '../models/user.models.js';
+
 export async function signup(req, res){
-    res.send("Signup route");
+    try {
+        const {email,password,username } = req.body;
+
+        if(!email || !password || !username) {
+            return res.status(400).json({success:false, message:'All fields are required'})
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(!emailRegex.test(email)) {
+            return res.status(400).json({success:false, message:'Invalid email'})
+        }
+        if(password.length < 6) {
+            return res.status(400).json({success:false, message:'Password must be at least 6 characters'})
+        }
+        const existingUserByEmail = await User.findOne({email:email})
+
+        if(existingUserByEmail){
+            return res.status(400).json({success:false, message:'Email already in use'})
+        }
+
+        const existingUserByUsername = await User.findOne({username:username})
+
+    } catch(error) {
+
+    }
 }
 
 export async function login(req, res){
