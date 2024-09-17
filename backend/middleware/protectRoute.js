84e6f593a5e0999f0cfc08e5d842805d64fd0364
Camber <cedge2@asu.edu>
2024-09-17 16:58:01 -0500

    Frontend implemented

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,31 +1,32 @@
-import jwt from 'jsonwebtoken';
-import { user } from '../models/user.models.js';
-import { ENV_VARS } from '../config/envVars.js';
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { ENV_VARS } from "../config/envVars.js";
 
-export const protectRoute = async (req, rez, next) => {
-    try {
-        const token = req.cookies[jwt-netflix]
-        if(!token) {
-            return res.status(401).json({success: false, message: 'Unauthorized - no token provided'});
-        }
+export const protectRoute = async (req, res, next) => {
+	try {
+		const token = req.cookies["jwt-netflix"];
 
-        const decode = jwt.verify(token, ENV_VARS.JWT_SECRET);
+		if (!token) {
+			return res.status(401).json({ success: false, message: "Unauthorized - No Token Provided" });
+		}
 
-        if(!decoded) {
-            return res.status(401).json({success: false, message: 'Unauthorizex - invalid token'});
-        }
+		const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET);
 
-        const user = await user.findByID(decoded.userId).select('-password');
+		if (!decoded) {
+			return res.status(401).json({ success: false, message: "Unauthorized - Invalid Token" });
+		}
 
-        if(!user) {
-            return res.status(404).json({success: false, message: 'User not found'});
-        }
+		const user = await User.findById(decoded.userId).select("-password");
 
-        req.user = user;
-        next();
+		if (!user) {
+			return res.status(404).json({ success: false, message: "User not found" });
+		}
 
-    } catch(error) {
-        console.log('Error in protectRoute middleware: ', error.message);
-        res.status(500).json({success: false, message: 'Internal server error'});
-    }
+		req.user = user;
+
+		next();
+	} catch (error) {
+		console.log("Error in protectRoute middleware: ", error.message);
+		res.status(500).json({ success: false, message: "Internal Server Error" });
+	}
 };
